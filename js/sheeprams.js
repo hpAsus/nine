d39222ef2441393336189480969ece771b5fc252
kaseopea <kaseopea@gmail.com>
2016-08-15 09:45:33 +0300

    updates

--- js/sheeprams.js
+++ js/sheeprams.js
@@ -14,7 +14,9 @@
 
     //clear guess input on click
     guessInput.addEventListener('click', function (ev) {
-        this.value = '';
+        if (this.value === '0000') {
+            this.value = '';
+        }
     });
 
     // only digits
@@ -30,7 +32,44 @@
 
     // PC Number Generator
     function generateNumber() {
-        return '0' + _.random(100,999);
+        return '0' + _.random(100, 999);
+    }
+
+    console.log(generateNumber());
+
+    var pcNumber = '0375';
+    var guess = '7145';
+
+    console.log(countSheepsNRams('0375', '7145'));
+    console.log(countSheepsNRams('0375', '7175'));
+
+    function countSheepsNRams(number, guess) {
+        var num1 = number.split('');
+        var num2 = guess.split('');
+        var results = {
+            sheeps: 0,
+            rams: 0
+        };
+        _.forEach(num2, function (digit) {
+            
+        });
+        /*var same = _.intersection(num1, num2);
+
+        console.log(same);
+        if (same.length) {
+            _.forEach(same, function (item) {
+                var pos1 = _.indexOf(num1, item);
+                var pos2 = _.indexOf(num2, item);
+                if (pos1 !== pos2) {
+                    results.sheeps++;
+                } else {
+                    results.rams++;
+                }
+            });
+        } else {
+            return 0;
+        }*/
+        return results;
     }
 
     // setInterval(function () {
